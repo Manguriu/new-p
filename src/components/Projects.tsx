2ec04e88d1e7d8315f9b4443984754ee777d4d3c
manguriu <brianmiruri983@gmail.com>
2023-07-23 14:56:42 +0300

    d

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -23,7 +23,7 @@ function Projects() {
             scale: 1,
           }}
           className=" rounded-lg mx-auto mb-3 md:mb-0 md:w-1/2 h-full"
-          src="/src/assets/react.png"
+          src="https://lh3.googleusercontent.com/pw/AIL4fc-9taM6c0l8UyN4N5e--3tgLeZ1sVNlzOnTauVA-P1FJgCR73JaXpwSRidMHYHlp07OOGtaAHM_xXyguk48EUR4IdMLmbeae6-OQNV0KXEdzYctjUQPTrcufOu3DCftO10OswBBMaW4-NRgzE4iThQEQWLYzmULcg5w-XlkWIl49yXAJ4h6J4NR9LauurIT45AXNf2KWEUnnwVmtsP7QB7Kkf2KiBPV-aDoK9MYHRe-5E60skGaGRI5WDoC4-nJYhmV2Os60fBpxh2pUNiAl49hngSuBysXAuqsGWeN0THGNIihM6pO65cLy3LXbtgncQWTf5X-tOZqfxbeEbC9SlJtQNwwJU8TxoRSqY_6UX6rZe_7uSyqc88sI9PMo3w4LPxDudzDn3LtCAJrzVoKeEmEKUuzk1TOleXaYfKLkh34Uhm85J0AgY-qPDkozmSbW1bDMV_cMJzG1lkj7p5aE-qlcCKOQs9bzCah0aEFs4uUYN0eJHIV1eA8CvQS_8-d5LFkqP-x0evjIGEaMeGICfoAprx0DoQemFa14ygAASsx4BOoKVhyXWOhFE3pGRh3YUz5tTmDqq7x7y7dQitzEPTNTbBLo5qNYQ0crvkbz9D4RPH4TsHflWGW2fE9fVzPeFTK2YSCg5kcWWZ2G3yi0ZOLLB_tIXG6Z_EDq2pvvnJsn-blwANhJY4Rvt5egGaTu4NVSjU9nigMwmWYmidCmzGkQkQOeaQlDwqjxHIWiKVV7MFNLE4dTURvpinSTRXGW-ig6RVki-t3nI732CjJWYCGxL918Vk2ON1F44NtxAJs9cMEScTr4pxZe-HMQ5ay21Y8yrBMv9fYVFhkULwJV8hVN7WAEmQt1DEY5kLqsNsY2Tm7Dk1GVm9OPwiB4F-1qGVQDNCCPFJ97yfeytfQLMt0TzRz0v4bVqshhlRb3opscv3ygbgi1-ex3B78nxs=w1769-h995-s-no?authuser=1"
           alt=""
         />
         <div className="summary ml-8">
@@ -61,7 +61,7 @@ function Projects() {
             scale: 1,
           }}
           className="rounded-lg mx-auto mb-3 md:mb-0 md:w-1/2 h-full"
-          src="/src/assets/lara.png
+          src="https://lh3.googleusercontent.com/pw/AIL4fc_fBaU4x0TLX4uUY197dbfTYpJ21I4-3B2_Bf0qGBztoHKs4tfvonZe9S8bUHt0P_n9zRnfaT1sfwzK_mMcygALzxu6iX2scsKdYS0MiphAVAa8NhyAO_3r5m5hKuy9LOVvf-__rvbhSns9_kziIiRvlttP4B_kT_PbpkC7PtUtD222w7scjTgdF3gcRyK_02ByVvy4Yqo23gil0dd9E7W0wacX7AbFWGqq9wG4fcVe4Oh9-DgIhrzt_z_rWZi_qQki5VaSSGKY5C3xgp-OlUV6UzX3sPJZgsC38LGfe8BrK9pc5EApQZBThe5wNx4wDpkXioBWCJCgAGE6fmN7SI-Q5jMQCReVlOPY1v2-9YMLQ20NXCS_qZNDoKcH7nofcgrVYDQBJC2KSPNXuQD7hjnpzmfAVGbuRmjsnzIBWlfvFGi07vanSAZh8DOtvJNFV8spSyKwUr3aImG666n7rY6IrulG0RCWv_1dPHLnJI_7e3fKrA_dG_-f5amBlNNa9ga2ikzF3RgsIEnFapwzJXTN2tqi80z2zfmZEVbNGiiRWuL64rQNo9vwroCHm2wo5XZu__YfbeMxaxJvD4usBDqXQaU3Gq2zHvqkpbJQXksmXPbMwnqNexczYg29_ClDSpPZ6a_mHa-XNMJ4V-p35JMK7MIXEgTecCVe5oCy0IqIX8037KkPVw0AkeZn5-edbJemW6Q5Ntu04qxRfe8wqutiP9Hxk8p68ItmztsEP5g2UHDCIQRt9rbellbUmmpwod2qgYAT7tA26Ezq6LrM6xv-KaEuHj4J-82xkn7ya-Oki1sAdATjUCLlzkrztoc3o6-6FkUNCRoblHbBMmm4t9CgDsDmT-fs99bqdMH0UIWgjKyq3pKGhOs3L1TVMccGvovdGHbtYQgGATfxjH1UusPIT1MTkuwDaJcxvCZsC22R8uEwKhl6Ti0S5xzMSgo=w718-h404-no?authuser=1
           "
           alt=""
         />
@@ -99,7 +99,7 @@ function Projects() {
             scale: 1,
           }}
           className="rounded-lg mx-auto mb-3 md:mb-0 md:w-1/2 h-full"
-          src="/src/assets/billing.png"
+          src="https://lh3.googleusercontent.com/pw/AIL4fc8GPEcLgkwzOH-Bm0fhaVVfCfq4dCPD8m0Vg4YBcrTAYAI5bgjwHtnkdNVpSDOTz6Ed82C4b_e6oxfUoC-8DrYSQ-BzRfg12CRwq1STkA3thiumhqe74YSX8UM4mdRAmS9dh7h-a49nY_vNvFL3LzA1maXO9kTbJaMSX8KihXALQ1frt0kvfMJNW3twtCF9p45PRdRzfIT8OqpIzcqDc6mynUF2QJEp1O-oBwixOIJZFEvPUDMfhm6n8krSqx18NnncMrJn5eOLkvMWVxvs-oAEwuxt-MTEy5TcMxJnEGNkdCN6rKrnPo2L5tDLZhJuAnArfjXNRxiwg8fOW9dVoiOrBs5pdM8UWF8t6s8G7DWngq4V_xOfbff6uHRewtivPr-knGuG01drz-ku9jLiUq12CFT4WuanMl_0-d7nlvH3ixIs36Uhje4rUGafhfuo58SFIzmyoZLo8qv06p3bk_MMo28ugsc2xiI5uIbrAcc1UulSMAZfLY5LGgAd8t0SryBAv67R3zIWR2rF3TN-sYwWIrDSH5w8bUfijGdF5YgmqwEwLujHBpCqB5jqTjSIWGvRwGc7-yXDTVeni7aL8SdJdf-iWlQz7E1WJDPI_NN2aBjDCKe7jBcVT2HrnRgvnFBVktgY73qoTQtLHR8tf10aBA0udvTlBeH2cnM6BoqzvqyoeBEHSDPpdhLD-C0PmATXkwpg2PqUUuk8iE_6VljyzWDgkxxnufHHSgQ_ferbC-rn7UENdV3fpq0pnhIWRALHGTB92jg8GFMGfQSANuHAIdUuAmdvCti-934Lus4B5pf2Nr2NuQvDxE_Jg8tsdcEIP0w8ZabME0XKE9g28pIrh5ykfD7glxUbFNQe9rC0GKCeLkj0DHK3sYwpFJxuOtk-Tm8oBdjac0itzvQKNximqFuDg-tghCHCzjX_8qA1-hE7wKmarkhnjsgILFY=w1769-h995-s-no?authuser=1"
           alt=""
         />
         <div className="summary ml-8">
--- src/components/SiteNav.tsx
+++ src/components/SiteNav.tsx
@@ -2,7 +2,10 @@ function SiteNav() {
   return (
     <div className="navbar">
       <div className="navbar-start navbar-container">
-        <img src="/src/assets/logo-aviato.png" width={20} />
+        <img
+          src="https://lh3.googleusercontent.com/pw/AIL4fc9_NPq9ckSiSyr6IMIqSZJm6Sktm0L8-ulbJGNVJrgQuBwSY1diKYk7Dwer5xW5HWVGew-xCMF3X1Wk78L4UFWISPRlKG-xptaxPud-dvMonWnOheVIRWwcUhf_BDISWhLdK-9J6m_5ApHX7uoyAn8dd_Q9JbpYMnzVa3k74t3BF51HnoSyd-8AWgFGEbA-w2svnbDYcFEYDt2qQlVgdRWkFt-eAzZmVYos7IvBBZtgVshVWEEgj9C3DbE8jjlE2WBTpaL7I4zDxaqcS_51b4QQMWOVFPMsYFU9eNf9t9JrtHQaHlpLTCnWc6Q9gUAzf_Rf5kOz0DHggep1Zw0KVTfqKqvn5cuNt8aIozqFhntjCrAirP7Bwj2wjZ4upQ-TV8T_AXC0oRlnrWhcImQrlb9uxXBb3O9Mg7HEaRNLuF3z1-6sbfczXmqSMyqSrOkOpbWDrmP7bXsghIEoO07F6sA1kHjx-fhtcvBKZge5EiPAq9Gu9GOatK7Ik6hfzRHJI-nL_KDsYY8guarajwgUEWB9fWKOb7i8ZktSITlcNxG85tLtWAJ3PxDAM2XGuld9gVwLUSUG-qShzXr87myXXAhMFgQfA_RAAiHGeqzfmrI-SWJ9LkH1k90f5nS3fH-UzIhvFrzULJG6ya-0ujQb04R-04mB4PWGSGWwE47eMV2wtwUQ7RQVMMN2GEqyp9OsVciBjQfnO_ZjgirSVxmgOC8apkdLlbEK1SN_4ca9palpk4SMitUvHbFnFX5Re55gGxO1iAv9otMJW9MBLd6HU4sefszQmevSVqNTqMUyyYovEycM5Fn9PWMvgkr-c5AZvbL7iBYolVZBp246AdwoQk_2d1vYJNlGYa_s0dNnO-2YGIJdBoYJ9eUvjGTKsku9AmY1xXdVfNEFfXyREOZClW6FgeSpBGZ4YnuaWLTrkjkTjBfp0rNeNRZVV4zMn-g=w63-h60-s-no?authuser=1"
+          width={20}
+        />
         <a href="/" className="btn btn-ghost normal-case text-xl navbar-logo">
           <meta
             name="viewport"
@@ -33,7 +36,7 @@ function SiteNav() {
       </div> */}
       <div className=" text-base navbar-end" role="none">
         <a
-          href="/src/assets/Resume-Brian-Manguriu.pdf"
+          href="https://drive.google.com/file/d/11TuqYkJHmzkwOhPIvYI97LwRtD8dwnnM/view?usp=drive_link"
           media="print and (resolution:300dpi)"
           target="_blank"
           role="none">
